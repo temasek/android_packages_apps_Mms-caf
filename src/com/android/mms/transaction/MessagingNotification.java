5746837a857f6f986033b5af85758a440c532a83
Bajee11 <bajee11@gmail.com>
2015-02-22 02:18:07 -0700

    [2/4] Mms: Breathing missedcall/sms/voicemail
    Base: https://gerrit.omnirom.org/#/c/4976/
    OmniGears: https://gerrit.omnirom.org/#/c/4978/
    Telephony: https://gerrit.omnirom.org/#/c/4979/
    
    Change-Id: Id12182719315a6e5bcbabd8f843eff70c8c3ed3a
    
    @alviteri: Port by me to crDroid Android

--- src/com/android/mms/transaction/MessagingNotification.java
+++ src/com/android/mms/transaction/MessagingNotification.java
@@ -57,6 +57,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.provider.BaseColumns;
+import android.provider.Settings;
 import android.provider.Telephony.Mms;
 import android.provider.Telephony.MmsSms;
 import android.provider.Telephony.Sms;
@@ -392,7 +393,7 @@ public class MessagingNotification {
         NotificationManager nm = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-//        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
+        // TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(context);
         // Update the notification.
         PendingIntent pendingIntent;
         if (subId == SubscriptionManager.INVALID_SUBSCRIPTION_ID) {
@@ -1173,7 +1174,12 @@ public class MessagingNotification {
                     mostRecentNotification.mClickIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         }
         // Always have to set the small icon or the notification is ignored
-        noti.setSmallIcon(R.drawable.stat_notify_sms);
+        if (Settings.System.getInt(context.getContentResolver(),
+                Settings.System.KEY_SMS_BREATH, 0) == 1) {
+               noti.setSmallIcon(R.drawable.stat_notify_sms_breath);
+           } else {    
+               noti.setSmallIcon(R.drawable.stat_notify_sms);
+        }
 
         NotificationManagerCompat nm = NotificationManagerCompat.from(context);
 
